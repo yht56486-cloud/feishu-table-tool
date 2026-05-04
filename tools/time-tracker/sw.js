f0d3598e159007fd1ad75d881a7d68d0e037f7e3
yht56486 <yht56486@gmail.com>
2026-05-04 19:16:26 +0800

    bump cache version to force refresh

--- tools/time-tracker/sw.js
+++ tools/time-tracker/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'time-tracker-v1';
+const CACHE = 'time-tracker-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
