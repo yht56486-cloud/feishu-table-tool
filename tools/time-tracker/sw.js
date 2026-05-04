759bec66a7d1561b4342bfdb0cd7f53830ad34d2
yht56486 <yht56486@gmail.com>
2026-05-04 19:07:52 +0800

    add time-tracker PWA

---
+++ tools/time-tracker/sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'time-tracker-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      caches.match('./index.html').then(r => r || fetch(e.request))
+    );
+  }
+});
